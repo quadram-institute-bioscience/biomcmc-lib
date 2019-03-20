7b4328c0b1cfe58a3660b46449aa1e2d3edbfd7c
Leonardo de-Oliveira-Martins (QIB) <Leonardo Martins leomrtns@gmail.com>
2019-03-20 17:33:35 +0000

    added dummy unit tests

---
+++ tests/check_unit.c
@@ -0,0 +1,12 @@
+#include <biomcmc.h> 
+
+#define TEST_SUCCESS 0
+#define TEST_FAILURE 1
+#define TEST_SKIPPED 77
+#define TEST_HARDERROR 99
+
+int
+main (int argc, char **argv)
+{
+  return TEST_SUCCESS; 
+}
